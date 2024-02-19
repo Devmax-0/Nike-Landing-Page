4e0f3f7ab10ed07b2a6ced8046bff1871ad8adb8
Devmax_ <muhammadhamzashahid31@gmail.com>
2024-02-20 01:29:51 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import {
+  Nav,
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from "./Components";
+
+const App = () => {
+  return (
+    <>
+      <main>
+        <Nav />
+        <section className="xl:padding-l wide:padding-r padding-b">
+          <Hero />
+        </section>
+        <section className="padding">
+          <PopularProducts />
+        </section>
+        <section className="padding">
+          <SuperQuality />
+        </section>
+        <section className="xl:padding-l wide:padding-r padding-b padding-r">
+          <Services />
+        </section>
+        <section className="padding-x py-10">
+          <SpecialOffer />
+        </section>
+        <section className="bg-pale-blue padding">
+          <CustomerReviews />
+        </section>
+        <section className="padding-x sm:py-32 py-16 w-full">
+          <Subscribe />
+        </section>
+        <section className="bg-black padding-x padding-t pb-8">
+          <Footer />
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default App;
